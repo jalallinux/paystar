7cea39550435c25a9bf76c962e33723166dea8d3
jalallinux <smjjalalzadeh93@gmail.com>
2023-02-08 22:07:13 +0330

    generate jwt secret. add uuid column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('name');
             $table->string('email')->unique();
             $table->unsignedBigInteger('balance')->default(0);
