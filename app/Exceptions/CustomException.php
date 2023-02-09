0be81681d2d4774971d3e5fcbe14d530ce19256e
jalallinux <smjjalalzadeh93@gmail.com>
2023-02-09 21:49:51 +0330

    fix status code of CustomException.

--- app/Exceptions/CustomException.php
+++ app/Exceptions/CustomException.php
@@ -3,8 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
+use Throwable;
 
 class CustomException extends Exception
 {
-    //
+    public function __construct($message = "", $code = 400, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
