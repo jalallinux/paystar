5a0bf7f56140b2c2b6a88f69c2c2361873146fc7
jalallinux <smjjalalzadeh93@gmail.com>
2023-02-10 16:21:18 +0330

    create PaymentController with index, store, show methods

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -2,36 +2,42 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Facades\Paystar;
-use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Payment\PaymentCallbackRequest;
+use App\Http\Requests\Api\Payment\PaymentIndexRequest;
+use App\Http\Requests\Api\Payment\PaymentStoreRequest;
 use App\Http\Resources\Api\Payment\PaymentResource;
-use App\Models\Payment;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use App\Models\Payment;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PaymentController extends Controller
 {
     /**
      * Display a listing of the payment.
      *
-     * @return \Illuminate\Http\Response
+     * @param PaymentIndexRequest $request
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function index(PaymentIndexRequest $request)
     {
-        //
+        $payments = $request->user()->payments()
+            ->orderBy($request->orderBy(), $request->orderType())
+            ->paginate($request->query('perPage'));
+        return PaymentResource::collection($payments);
     }
 
     /**
      * Store a newly created payment in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param PaymentStoreRequest $request
      * @return PaymentResource
      */
-    public function store(Request $request)
+    public function store(PaymentStoreRequest $request)
     {
-        /** @var Payment $payment */
-        $payment = $request->user()->payments()->create([
-            'amount' => $request->input('amount'),
-        ]);
+        $payment = $request->user()->payments()
+            ->create($request->safe()->only('amount'));
 
         return new PaymentResource($payment);
     }
@@ -39,22 +45,28 @@ class PaymentController extends Controller
     /**
      * Display the specified payment.
      *
+     * @param Request $request
      * @param Payment $payment
-     * @return \Illuminate\Http\Response
+     * @return PaymentResource
+     * @throws \Throwable
      */
-    public function show(Payment $payment)
+    public function show(Request $request, Payment $payment)
     {
-        //
+        throw_if(
+            $payment->user->id != $request->user()->id,
+            new AuthorizationException
+        );
+        return new PaymentResource($payment);
     }
 
     /**
      * Callback the specified payment in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param PaymentCallbackRequest $request
      * @param Payment $payment
      * @return \Illuminate\Http\Response
      */
-    public function callback(Request $request, Payment $payment)
+    public function update(PaymentCallbackRequest $request, Payment $payment)
     {
         $request->dd();
     }
---
+++ app/Http/Requests/Api/Payment/PaymentIndexRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests\Api\Payment;
+
+use App\Http\Requests\Api\PaginateAndOrderableRequest;
+
+class PaymentIndexRequest extends PaginateAndOrderableRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function orderedColumns(): array
+    {
+        return ['id', 'amount', 'created_at', 'updated_at'];
+    }
+}
