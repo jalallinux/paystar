c5bb2bf50421564a6df514e40631da0f37bcfdea
jalallinux <smjjalalzadeh93@gmail.com>
2023-02-10 16:41:05 +0330

    fix exception message

--- lang/en/exceptions.php
+++ lang/en/exceptions.php
@@ -5,6 +5,6 @@ return [
     'NotFoundHttpException' => 'آدرس مورد نظر یافت نشد.',
     'ModelNotFoundException' => ':attribute مورد نظر یافت نشد.',
     'ValidationException' => 'اطلاعات وارد شده صحیح نمی باشد.',
-    'AuthorizationException' => 'دسترسی به این قسمت ندارید.',
+    'AuthorizationException' => 'دسترسی به این درخواست ندارید.',
     'ThrottleRequestsException' => 'درخواست های شما بیش از حد مجاز می باشد، لطفا دقایقی دیگر تلاش کنید.',
 ];
